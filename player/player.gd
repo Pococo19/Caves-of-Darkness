44b9fd3e3870798583744b44eeb99912fe2f948c
Pococo19 <luka.vujanovic@epitech.eu>
2026-02-13 13:11:33 +0100

    ADD wall slide

--- player/player.gd
+++ player/player.gd
@@ -7,12 +7,12 @@ const SPEED = 300
 const SLIDE_SPEED = 510
 const JUMP = -400
 
-enum State { Idle, Run, Jump, Fall, Attack, Slide, Wall }
+enum State { Idle, Run, Jump, Fall, Attack, Slide, Wall, Special }
 
 var current_state
 
 func _on_animation_finished():
-	if current_state == State.Attack or current_state == State.Slide:
+	if current_state == State.Attack or current_state == State.Slide or current_state == State.Special:
 		current_state = State.Idle
 
 func _ready():
@@ -26,6 +26,7 @@ func _physics_process(_delta):
 	player_run(_delta)
 	player_jump(_delta)
 	player_attack(_delta)
+	player_special(_delta)
 	move_and_slide()
 	
 	player_animations()
@@ -42,12 +43,12 @@ func player_falling(_delta):
 	if !is_on_floor():
 		velocity.y += grav * _delta
 
-	if velocity.y > 0 and current_state != State.Attack and current_state != State.Wall:
+	if velocity.y > 0 and current_state != State.Attack and current_state != State.Special and current_state != State.Wall:
 		current_state = State.Fall
 		anims.flip_h = false if direction > 0 else true
 
 func player_idle(_delta):
-	if is_on_floor() and current_state != State.Attack  and current_state != State.Slide:
+	if is_on_floor() and current_state != State.Attack and current_state != State.Special  and current_state != State.Slide:
 		current_state = State.Idle
 		
 func player_run(_delta):
@@ -60,11 +61,11 @@ func player_run(_delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	if is_on_floor() and direction != 0 and current_state != State.Attack  and current_state != State.Slide:
+	if is_on_floor() and direction != 0 and current_state != State.Attack and current_state != State.Special  and current_state != State.Slide:
 		current_state = State.Run
 		anims.flip_h = false if direction > 0 else true
 		
-	if Input.is_action_just_pressed("slide") and current_state != State.Attack and direction != 0 and is_on_floor():
+	if Input.is_action_just_pressed("slide") and current_state != State.Attack and current_state != State.Special and direction != 0 and is_on_floor():
 		current_state = State.Slide
 		velocity.x = direction * SLIDE_SPEED
 		anims.flip_h = direction < 0
@@ -72,7 +73,7 @@ func player_run(_delta):
 func player_jump(_delta):
 	if current_state == State.Wall:
 		return
-	if Input.is_action_just_pressed("jump") and current_state != State.Attack and is_on_floor():
+	if Input.is_action_just_pressed("jump") and current_state != State.Attack and current_state != State.Special and is_on_floor():
 		velocity.y = JUMP
 		current_state = State.Jump
 	
@@ -114,6 +115,29 @@ func player_attack(_delta):
 	if current_state != State.Attack:
 		anims.rotation_degrees = 0
 
+func player_special(_delta):
+	var direction = Input.get_axis("move_left", "move_right")
+
+	if Input.is_action_just_pressed("special") and current_state != State.Special:
+		current_state = State.Special
+		
+		# DOWN ATTACK en l'air
+		if !is_on_floor() and Input.is_action_pressed("move_down"):
+			print("DATTAACK")
+			
+			# rotation selon la direction
+			if anims.flip_h:
+				anims.rotation_degrees = -50   # gauche
+			else:
+				anims.rotation_degrees = 50  # droite
+				
+		else:
+			anims.rotation_degrees = 0
+
+	# remettre la rotation quand l’attaque est finie
+	if current_state != State.Special:
+		anims.rotation_degrees = 0
+
 	
 func player_animations():
 	if current_state == State.Idle:
@@ -126,6 +150,8 @@ func player_animations():
 		anims.play("fall")
 	elif current_state == State.Attack:
 		anims.play("attack_1")
+	elif current_state == State.Special:
+		anims.play("attack_2")
 	elif current_state == State.Slide:
 		anims.play("slide")
 	elif current_state == State.Wall:
