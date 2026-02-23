dd96ad37ba65663711965be3bc5f4e42e31bed26
Luka Vujanovic <luka.vujanovic@epitech.eu>
2026-02-23 13:46:43 +0100

    [ADD] better menu image

--- script/main_menu.gd
+++ script/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://level1.tscn")
+	get_tree().change_scene_to_file("res://levels/testlevel1.tscn")
 
 func _on_options_button_pressed():
 	print("Options menu not yet implemented")
